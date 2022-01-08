bc5755453e619ae645babb43936aca64bd44c2ad
Laura Kapitza <laura.kapitza@gmail.com>
2022-01-08 17:42:08 +0100

    first commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
